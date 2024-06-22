974cf4065d7e61f813e3cd2757f1d78a0878cfb9
solituude <dbelousova70@gmail.com>
2024-06-22 17:09:30 +0400

    Create project, install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import LogPage from "./pages/LogPage/LogPage";
+import MapPage from "./pages/MapPage/MapPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LogPage/>}/>
+                    <Route path='/statement' element={<LogPage/>}/>
+                    <Route path='/map/<x>/<y>/<zoom>' element={<MapPage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/LogPage/LogPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LogPage: React.FC = () => {
+    return(
+        <></>
+    );
+}
+
+export default LogPage;
